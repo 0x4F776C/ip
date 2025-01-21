46807098b62de4e2d37f72498b1711f55328a9b6
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2025-01-21 22:48:55 +0800

    Level-3 done

--- src/main/java/TrashBot.java
+++ src/main/java/TrashBot.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
 
 public class TrashBot {
-    private static String[] tasks = new String[100]; // Task array
-    private static int taskCount = 0;  // Keep track of number of tasks
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
 
-    // Method for responding to user input
     private static void respond(String input) {
-        if (input.equals("bye")) {
+        String[] divider = input.split(" ", 2); // differentiate dInput and number (if exist)
+        String dInput = divider[0].toLowerCase(); // get the mark/unmark part of the input
+
+        if (dInput.equals("bye")) {
             System.out.println("____________________________________________________________\n" +
                     " Bye. Don't cross the road with your eyes closed!\n" +
                     "____________________________________________________________\n");
-            System.exit(0);  // Exit the program when "bye" is entered
+            System.exit(0);
         }
-        else if (input.equals("list")) {
+        else if (dInput.equals("list")) {
             if (taskCount == 0) {
                 System.out.println("List is empty!");
                 return;
@@ -23,9 +25,47 @@ public class TrashBot {
             }
             System.out.println("______________________________________________________________");
         }
+        else if (dInput.equals("mark")) {
+            if (divider.length < 2) {
+                System.out.println("Please specify the task number to mark as mark!");
+                return;
+            }
+            try {
+                int taskNum = Integer.parseInt(divider[1]) - 1;
+                if (taskNum >= 0 && taskNum < taskCount) {
+                    tasks[taskNum].markAsDone();
+                    System.out.println("Nice! I've marked this task as mark:");
+                    System.out.println("  " + tasks[taskNum]);
+                } else {
+                    System.out.println("Invalid task number!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid task number!");
+            }
+        }
+
+        else if (dInput.equals("unmark")) {
+            if (divider.length < 2) {
+                System.out.println("Please specify the task number to mark as not mark!");
+                return;
+            }
+            try {
+                int taskNum = Integer.parseInt(divider[1]) - 1;
+                if (taskNum >= 0 && taskNum < taskCount) {
+                    tasks[taskNum].markAsNotDone();
+                    System.out.println("Okay, I've marked this task as not mark:");
+                    System.out.println("  " + tasks[taskNum]);
+                } else {
+                    System.out.println("Invalid task number!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid task number!");
+            }
+        }
         else {
             if (taskCount < tasks.length) {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
+                System.out.println("Added: " + tasks[taskCount]);
                 taskCount++;
             } else {
                 System.out.println("List is full!");
@@ -37,18 +77,15 @@ public class TrashBot {
         String welcomeText = "____________________________________________________________\n" +
                 " Hello! I'm TrashBot\n" +
                 " What can I do you for?\n" +
-                "____________________________________________________________\n"; // Variable for welcome string
-
-        System.out.println(welcomeText); // Print out welcome string
-
-        Scanner scanInput = new Scanner(System.in); // Create Scanner object
-
+                "____________________________________________________________\n";
+        System.out.println(welcomeText);
+        Scanner scanInput = new Scanner(System.in);
         while (true) {
-            String userInput = scanInput.nextLine(); // Get user input
+            String userInput = scanInput.nextLine();
             if (userInput.isEmpty()) {
-                continue;  // Skip empty inputs
+                continue;
             }
-            respond(userInput); // Respond according to input match
+            respond(userInput);
         }
     }
 }
