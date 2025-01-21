b906596599c5fe9c9bcd9087b4f8bb1da65d0a0d
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2025-01-21 21:58:54 +0800

    Level-1 done

--- src/main/java/TrashBot.java
+++ src/main/java/TrashBot.java
@@ -1,10 +1,37 @@
+import java.util.Scanner;
+
 public class TrashBot {
+    // Method for responding to user input
+    public static void respond(String input) {
+        if (input.equals("bye")) {
+            System.out.println("____________________________________________________________\n" +
+                    " Bye. Don't cross the road with your eyes closed!\n" +
+                    "____________________________________________________________\n");
+        }
+        else {
+            System.out.println("____________________________________________________________\n" +
+                    input +
+                    "\n" +
+                    "____________________________________________________________\n");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
+        // Variable for welcome text
+        String welcomeText = "____________________________________________________________\n" +
                 " Hello! I'm TrashBot\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
+                " What can I do you for?\n" +
+                "____________________________________________________________\n";
+
+        // Print out welcome text
+        System.out.println(welcomeText);
+
+        // Create Scanner object
+        Scanner scanInput = new Scanner(System.in);
+        // Get user input
+        String userInput = scanInput.nextLine();
+
+        // Get user input and respond accordingly
+        respond(userInput);
     }
 }
