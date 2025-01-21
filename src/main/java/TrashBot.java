235bb2634a3642c710c14714e3f253fee6f3652d
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2025-01-21 21:30:21 +0800

    Level-0 done

---
+++ src/main/java/TrashBot.java
@@ -0,0 +1,10 @@
+public class TrashBot {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm TrashBot\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+    }
+}
