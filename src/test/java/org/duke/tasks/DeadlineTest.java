80379491cc7efdbece7a051c18669ec95cfd6515
0x4F776C <41299140+0x4F776C@users.noreply.github.com>
2025-01-28 03:37:28 +0800

    A-Gradle and A-JUnit Done

---
+++ src/test/java/org/duke/tasks/DeadlineTest.java
@@ -0,0 +1,38 @@
+package org.duke.tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadlineTest {
+    @Test
+    void getDateTime_ValidFormat1() {
+        Deadline deadline = new Deadline("deadline submit report /by Sep 11 2023 1:33am");
+        assertEquals("Sep 11 2023 1:33am", deadline.getDateTime());
+    }
+
+    @Test
+    void getDateTime_ValidFormat2() {
+        Deadline deadline = new Deadline("deadline submit report /by 2023-09-11 0133");
+        assertEquals("Sep 11 2023 1:33am", deadline.getDateTime());
+    }
+
+    @Test
+    void getDateTime_InvalidFormat() {
+        String invalidDateTime = "invalid-date-format";
+        Deadline deadline = new Deadline("deadline submit report /by " + invalidDateTime);
+        assertEquals(invalidDateTime, deadline.getDateTime());
+    }
+
+    @Test
+    void getDateTime_EmptyDate() {
+        Deadline deadline = new Deadline("deadline submit report /by ");
+        assertEquals("", deadline.getDateTime());
+    }
+
+    @Test
+    void getDateTime_PartialDate() {
+        String partialDate = "Sep 11";
+        Deadline deadline = new Deadline("deadline submit report /by " + partialDate);
+        assertEquals(partialDate, deadline.getDateTime());
+    }
+}
